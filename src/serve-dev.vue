1122424a295e8b8591ec338c0bae7392ae8d2dc4
Mark Barr <mark@hyperingenuity.com>
2020-01-13 14:15:52 -0500

    vue-math-field: refactor to use rollup for publishing, version bump to v0.3.0

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueMathField from '@/vue-math-field.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueMathField
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-math-field />
+  </div>
+</template>
